3c08e8916b0e2dfdda2a08a21364c52fe242dfec
dnorkotube-oss <dnorkotube@gmail.com>
2026-03-16 09:35:24 +0500

    Update app.py

--- app.py
+++ app.py
@@ -143,4 +143,4 @@ def getlist():
     return jsonify(messages)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='127.0.0.1', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=8001)
