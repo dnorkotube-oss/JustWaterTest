eed2e7136d3a1c74062a47d7b1c28c6c16943fec
dnorkotube-oss <dnorkotube@gmail.com>
2026-03-16 10:23:00 +0500

    Update app.py

--- app.py
+++ app.py
@@ -141,6 +141,3 @@ def getlist():
     db.close()
     messages = [row['text'] for row in rows]
     return jsonify(messages)
-
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=8001)
